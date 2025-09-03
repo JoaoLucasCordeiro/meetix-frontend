bc7555f7554270e5224b005950b223d2d0d3994d
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-09-03 11:15:32 -0300

    feat: componente Team adicionado a Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/shared/landing-page/About"
 import Hero from "@/components/shared/landing-page/Hero"
+import Team from "@/components/shared/landing-page/Team"
 
 const LandingPage = () => {
   return (
     <main className="min-h-screen flex flex-col">
       <Hero />
       <About/>
+      <Team/>
     </main>
   )
 }
