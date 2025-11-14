3fe024c4eb6bc5cbff61cf22b59a6ad36e4cbacd
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-11-14 10:22:57 -0300

    feat: novas rotas aplicadas no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+  import Register from "./pages/Signup";
+import EventsPage from "./pages/EventsPage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import CreateEventPage from "./pages/CreateEventPage";
+import MyEventsPage from "./pages/MyEventsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/cadastro" element={<Signup />} />
+      <Route path="/cadastro" element={<Register />} />
+      <Route path="/eventos" element={<EventsPage />} />
+      <Route path="/eventos/:id" element={<EventDetailsPage />} />j
+      <Route path="/criar-evento" element={<CreateEventPage />} />
+      <Route path="/meus-eventos" element={<MyEventsPage />} />
+      <Route path="/opcoes" element={<SettingsPage />} />
     </Routes>
   );
 }
